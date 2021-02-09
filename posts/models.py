d5b4d315682a8e7c46731273a9e1c73b5915f296
SauravJalui <saurav.jalui-codes@outlook.com>
2021-02-09 08:32:17 +0530

    added docstrings

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
+    '''This contains the data to be published as a post in the message board app'''
     text = models.TextField()
 
     def __str__(self):
--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView
 from .models import Post
 
 class home_page_view(ListView):
+    '''This contains the data to be displayed on the home page'''
     model = Post
     template_name = 'home.html'
     context_object_name = 'all_posts_list'
